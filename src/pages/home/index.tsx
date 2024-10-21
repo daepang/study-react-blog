cef3584510c6946979ce196280a4347041f82373
daepang <dev.daepang@gmail.com>
2024-10-22 00:19:35 +0900

    post css 적용

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,11 +11,39 @@ export default function Home () {
                     <Link to="/proflie">프로필</Link>
                 </div>
             </header>
-            <div className="post-list">Post List</div>
+            <div className={"post__navigation"}>
+                <div className={"post__navigation--active"}>전체</div>
+                <div>나의 글</div>
+            </div>
+            <div className="post__list">
+                {[...Array(10)].map((e, index) => (
+                    <div key={index} className="post__box">
+                        <Link to={`/posts/${index}`}>
+                            <div className="post__profile-box">
+                                <div className="post__profile" />
+                                <div className="post_author-name">DAEPANG</div>
+                                <div className="post__date">2024.10.22 화요일</div>
+                            </div>
+                            <div className="post__title">게시글 {index}</div>
+                            <div className="post__text">
+                                test text. test text. test text. test text. test text.
+                                test text. test text. test text. test text. test text.
+                                test text. test text. test text. test text. test text.
+                                test text. test text. test text. test text. test text.
+                                test text. test text. test text. test text. test text.
+                            </div>
+                            <div className="post__utils-box">
+                                <div className="post__delete">삭제</div>
+                                <div className="post__edit">수정</div>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
             <footer>
-                <div>Menu 1</div>
-                <div>Menu 2</div>
-                <div>Menu 3</div>
+                <Link to="/posts/new">글쓰기</Link>
+                <Link to="/posts">게시글</Link>
+                <Link to="/proflie">프로필</Link>
             </footer>
         </div>
     );
